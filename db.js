718569d589ddae9b3ec6f200d60468fdb49b2eaf
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-08-29 23:59:33 +0600

    added error handling

--- db.js
+++ db.js
@@ -3,3 +3,6 @@ mongoose.connect('mongodb://localhost:27017/backend-server-side')
 mongoose.connection.on('connected', () => {
     console.log('connected to mongodb')
 })
+mongoose.connection.on('error', (err) => {
+    console.log('connection error : ', err)
+})
