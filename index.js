5e3aa7ab02667f3deece60d60b036af1310e5c94
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-08-29 21:49:54 +0600

    fixed error

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const app = express()
+
+app.use(bodyParser.json())
+app.use(cors())
+
+const port = process.env.PORT || 1200
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
