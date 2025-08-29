b6ac63dd0b19ef00fa4e194d49e6942ff4cd3d35
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-08-29 23:40:28 +0600

    added mongoose

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ app.use(bodyParser.json())
 app.use(cors())
 
 const port = process.env.PORT || 1200
-
+app.get('/', (req, res) => {
+    res.send("Hello World From Server")
+})
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
